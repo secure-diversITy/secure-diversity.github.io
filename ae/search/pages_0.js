4226b01cfd8df69fbbd8a7cf3f9491180bc46cae
TF <mail@sedi.one>
2021-01-23 18:13:34 +0100

    initial documentation for PORS instead of AE

--- ae/search/pages_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ansible_20engine_20_28ae_29',['ansible engine (ae)',['../index.html',1,'']]],
-  ['about_20_2f_20readme',['About / README',['../md__opt_data_development_git_ansible_engine_dmc_README.html',1,'']]]
-];
---
+++ pors/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['about_20_2f_20readme_0',['About / README',['../md__opt_data_development_git_ansible_engine_dmc_README.html',1,'']]]
+];
